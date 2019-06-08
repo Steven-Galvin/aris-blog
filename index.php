8c98f3a0d172c8970200a6a312ca1d7a990cb008
Steven Galvin <steven.john.galvin@gmail.com>
2019-06-07 18:27:47 -0700

    initial setup of user side php blog

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php require_once('config.php') ?>
+
+<!DOCTYPE html>
+<?php require_once('includes/head_section.php') ?>
+<html>
+<body>
+	<!-- container - wraps whole page -->
+	<div class="container">
+		<!-- navbar -->
+    <?php require_once('includes/navbar.php') ?>
+		<!-- // navbar -->
+
+    <!-- Banner -->
+    <?php include('includes/banner.php') ?>
+    <!-- // Banner -->
+
+		<!-- Page content -->
+		<main>
+			<h2 class="content-title">Recent Articles</h2>
+			<hr>
+			<!-- more content still to come here ... -->
+		</main>
+		<!-- // Page content -->
+
+		<!-- footer -->
+    <?php require_once('includes/footer.php') ?>
+		<!-- // footer -->
+
+	</div>
+	<!-- // container -->
+</body>
+</html>
