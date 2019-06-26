4c30e0ef8ca3eb5ec348df86c8938f90d5cb8c8b
Steven Galvin <steven.john.galvin@gmail.com>
2019-06-25 21:48:08 -0700

    add public functions to pull both posts and topics from the database and then display them on the index page. add styles to displayed posts.

--- config.php
+++ config.php
@@ -1,9 +1,12 @@
 <?php
 	session_start();
-
 	// connect to database
-       // coming soon...
+	$conn = mysqli_connect("127.0.0.1", "root", "K4lsifer290", "complete-blog-php");
 
+	if (!$conn) {
+		die("Error connecting to database: " . mysqli_connect_error());
+	}
+	// define global constants
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
 	define('BASE_URL', 'http://localhost/aris-blog/');
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php require_once('config.php') ?>
+<?php require_once( ROOT_PATH . '/includes/public_functions.php') ?>
+
+<!-- Retrieve all posts from database  -->
+<?php $posts = getPublishedPosts(); ?>
 
 <!DOCTYPE html>
 <?php require_once('includes/head_section.php') ?>
@@ -15,10 +19,30 @@
     <!-- // Banner -->
 
 		<!-- Page content -->
-		<main>
+		<main class="content">
 			<h2 class="content-title">Recent Articles</h2>
 			<hr>
 			<!-- more content still to come here ... -->
+			<?php foreach ($posts as $post): ?>
+				<div class="post" style="margin-left: 0px;">
+					<img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
+					<?php if (isset($post['topic']['name'])): ?>
+						<a href="<?php echo BASE_URL . 'filtered_posts.php?topic=' . $post['topic']['id'] ?>"
+							class="btn category">
+							<?php echo $post['topic']['name'] ?>
+						</a>
+					<?php endif ?>
+					<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
+						<div class="post_info">
+							<h3><?php echo $post['title'] ?></h3>
+							<div class="info">
+								<span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
+								<span class="read_more">Read more...</span>
+							</div>
+						</div>
+					</a>
+				</div>
+			<?php endforeach ?>
 		</main>
 		<!-- // Page content -->
 
